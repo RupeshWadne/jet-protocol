20d509b8ee3aaafc351374a680f8cc13b3cef5e1
shubham_wadne <kaylekashubham@gmail.com>
2023-08-02 09:04:22 +0530

    init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,124 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { motion } from "framer-motion";
+const src = [
+  "./i1.png",
+  "./i2.png",
+  "./i3.png",
+  "./i4.png",
+  "./i5.png",
+  "./i6.png",
+  "./i7.png",
+  "./i8.png",
+  "./i9.png",
+  "./i10.png",
+  "./i11.png",
+  "./i12.png",
+]
 
-const inter = Inter({ subsets: ['latin'] })
+const container = {
+  hidden: { opacity: 1, scale: 0 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delayChildren: 2,
+      staggerChildren: 0.5
+    }
+  }
+};
 
-export default function Home() {
+const item = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1
+  }
+};
+
+const Home = () => {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <>
+      <motion.div
+        variants={container}
+        initial="hidden"
+        animate="visible" 
+        className="flex font-Rampart flex-col justify-center items-center">
+        <motion.div variants={item} className="absolute top-[23px] left-[calc(50%_-_521px)] rounded-[80px] box-border w-[1042px] flex flex-col items-center justify-center text-left text-base text-white border-[1px] border-solid border-gray-800">
+          <div className="-z-10 self-stretch [backdrop-filter:blur(12px)] flex flex-row py-3 px-10 items-center justify-between">
+            <img
+              className="relative w-[119.14px] h-6 object-cover"
+              alt=""
+              src="/i15.png"
             />
-          </a>
-        </div>
-      </div>
+            <div className="flex flex-row items-center justify-center gap-[40px]">
+              <div className="relative lowercase">Products</div>
+              <div className="relative lowercase">DAO</div>
+              <div className="relative lowercase">Build</div>
+              <div className="relative lowercase">Docs</div>
+            </div>
+            <div className="rounded-[80px] bg-emerald-600 shadow-[0px_-2px_2px_rgba(0,_0,_0,_0.48)_inset,_0px_2px_2px_rgba(255,_255,_255,_0.48)_inset] flex flex-row py-2 px-5 items-center justify-center text-center text-sm text-black border-[1px] border-solid border-gray-600">
+              <div className="relative lowercase font-semibold">Launch App</div>
+            </div>
+          </div>
+        </motion.div>
+        <motion.img variants={item} src="/gradient.png" />
+        <motion.div
+        variants={item}
+         className="absolute top-[245px] flex flex-col items-center justify-start">
+          <div className="flex flex-col items-center justify-start gap-[40px]">
+            <div className="flex flex-col items-center justify-start gap-[20px]">
+              <div className="flex flex-col items-center justify-start">
+                <div  className="relative font-medium text-xs text-emerald-500">JET PROTOCOL</div>
+                <div className="relative text-[29px] text-white inline-block w-[607px]">
+                  <span className="leading-[126%] flex flex-col justify-center items-center">
+                    <span className="font-medium text-[2.5rem] p-3">{`the next generation of `}</span>
+                    <img src="defi.png" className="w-[75%]" alt="" />
+                  </span>
+                </div>
+              </div>
+              <div className="w-[80%] text-center relative text-xl text-gray-500">
+                experience open source, transparent and efficient borrowing and
+                lending on solana.
+              </div>
+            </div>
+            <div className="flex flex-row items-start justify-start gap-[20px] text-sm text-black">
+              <div className="rounded-[81px] bg-emerald-600 shadow-[0px_-2px_2px_rgba(0,_0,_0,_0.48)_inset,_0px_2px_2px_rgba(255,_255,_255,_0.48)_inset] flex flex-row py-2 px-5 items-center justify-center border-[1px] border-solid border-gray-600">
+                <div className="relative lowercase font-semibold">Read Docs</div>
+              </div>
+              <div className="rounded-[81px] bg-black shadow-[0px_-2px_2px_rgba(0,_0,_0,_0.48)_inset,_0px_2px_2px_rgba(255,_255,_255,_0.48)_inset] [backdrop-filter:blur(12px)] flex flex-row py-2 px-5 items-center justify-center text-white border-[1px] border-solid border-gray-400">
+                <div className="relative font-medium">how it works</div>
+              </div>
+            </div>
+          </div>
+        </motion.div>
+        <motion.div variants={item} className="relative flex overflow-x-hidden m-5 w-[80%]">
+          <div className="flex flex-row items-start justify-start gap-[24px] opacity-[0.6] py-12 animate-marquee whitespace-nowrap">
+            {src.map((src) => {
+              return (
+                <img
+                  key={src}
+                  className="relative w-[104.4px] h-12 object-cover z-[0]"
+                  alt=""
+                  src={src}
+                />
+              )
+            })}
+          </div>
+          <div className="absolute top-0 flex flex-row items-start justify-start gap-[24px] opacity-[0.6] py-12 animate-marquee2 whitespace-nowrap">
+            {src.map((src) => {
+              return (
+                <img
+                  key={src}
+                  className="relative w-[104.4px] h-12 object-cover z-[0]"
+                  alt=""
+                  src={src}
+                />
+              )
+            })}
+          </div>
+        </motion.div>
+      </motion.div>
+    </>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Home;
---
+++ src/scroll.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+const src = [
+    "./i1.png",
+    "./i2.png",
+    "./i3.png",
+    "./i4.png",
+    "./i5.png",
+    "./i6.png",
+    "./i7.png",
+    "./i8.png",
+    "./i9.png",
+    "./i10.png",
+    "./i11.png",
+    "./i12.png",
+  ]
+const scroll = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default scroll
